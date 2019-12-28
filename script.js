1e1ed2d068b8283d84996420f7afe6af883eb760
blinkjuan82 <ceratio24@gmail.com>
2019-12-28 12:24:04 -0700

    Got the random lower / upper case letters, symbols, and numbers to work

--- script.js
+++ script.js
@@ -11,7 +11,32 @@ const howLong = prompt("How long is your password? \n (Must be between 8 to 128 
             const capChar = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
             const capLength = capChar.length;
             const randChar = capChar[Math.floor(Math.random() * capLength)]
-                console.log(randChar);
+                console.log(randChar) 
+                //the random capital letter is working!!!;
+            if(capLett){
+                const lowLett = confirm("Does it contain lower case letters?");
+                const lowChar = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+                const lowLength = lowChar.length;
+                const randLow = lowChar[Math.floor(Math.random()* lowLength)];
+                console.log(randLow);
+                //random low letter is working!!!
+                if(lowLett){
+                    const numQues = confirm("Does it contain numeric characters?");
+                    const num =['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+                    const numLength = num.length;
+                    const randNum = num[Math.floor(Math.random() * numLength)];
+                    console.log(randNum);
+                    //random number working!!
+                    if(numQues){
+                        const symbolQues = confirm("Does it contain symbols??");
+                        const symbols = ['!', '@', '#', '$', '%', '^', '&', '*', '=', '-'];
+                        const symLength = symbols.length;
+                        const randSym = symbols[Math.floor(Math.random()) * symLength];
+                        console.log(randSym);
+                        //random symbols working!!!
+                    }
+                }
+            }
             
             
 
